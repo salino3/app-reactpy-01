1a9819961df21d1df2a1103bfb1f3dd7ca5d1f46
salino3 <flaviobcn012@gmail.com>
2023-06-19 08:47:38 +0200

    changing styles

--- new_component.py
+++ new_component.py
@@ -1,9 +1,11 @@
 from reactpy import component, html;
 from reactpy.backend.fastapi import configure;
-from fastapi import FastAPI;
+
 
 @component
 def Item2(text):
-  return html.li(text)
+  myStyle = {"style":  { "color": "violet", "font-size": "32px"}}
+
+  return html.li(myStyle, text)
 
 export = Item2
